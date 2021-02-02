f1b84f4cfc31c80c13ba6077d54c28f94d52cf8c
Pablo Luque <pablols114@gmail.com>
2021-02-02 19:07:08 +0100

    First attempt at making the init

--- callacalla/__init__.py
+++ callacalla/__init__.py
@@ -1,13 +1,10 @@
 import json
 from pathlib import Path
 
-from redbot.core.bot import Red
-
-from .{{cookiecutter.package_name}} import {{cookiecutter.cog_class_name}}
+from .callacalla import CallaCalla
 
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
-
-async def setup(bot: Red) -> None:
-    bot.add_cog({{cookiecutter.cog_class_name}}(bot))
+def setup(bot):
+    bot.add_cog(CallaCalla(bot))
