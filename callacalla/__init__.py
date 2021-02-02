878402c785f2c2f56cd6dfb4f5536453f94fbb6a
Pablo Luque <pablols114@gmail.com>
2021-02-02 18:35:13 +0100

    Added callacalla from template

---
+++ callacalla/__init__.py
@@ -0,0 +1,13 @@
+import json
+from pathlib import Path
+
+from redbot.core.bot import Red
+
+from .{{cookiecutter.package_name}} import {{cookiecutter.cog_class_name}}
+
+with open(Path(__file__).parent / "info.json") as fp:
+    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
+
+
+async def setup(bot: Red) -> None:
+    bot.add_cog({{cookiecutter.cog_class_name}}(bot))
